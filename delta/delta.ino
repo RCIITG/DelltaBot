9b68944e9fac93f9cc72d60ce81dda088eb48f1b
Sai Karthikey Pentapati <karthikey97@gmail.com>
2017-03-31 15:17:16 +0530

    Uploaded new python and arduino code

---
+++ delta/delta.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+Servo servo0;
+Servo servo1;
+Servo servo2;
+
+// create array
+int incoming[3];
+
+void setup(){
+  Serial.begin(9600);
+  Serial1.begin(9600);
+
+  servo0.attach(2);
+  servo1.attach(3);
+  servo2.attach(4);
+
+  servo0.write(70);
+  servo1.write(20);
+  servo2.write(75);   
+
+  pinMode(13,OUTPUT);
+  digitalWrite(13,LOW);
+}
+
+void loop(){
+  while(Serial.available() >= 3){
+    for (int i = 0; i < 3; i++){
+      incoming[i] = Serial.read();
+    }
+    // use the values
+    
+    servo0.write(incoming[0]+60);
+    servo1.write(incoming[1]+10);
+    servo2.write(incoming[2]+75);
+  }
+}
